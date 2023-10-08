286d567361f67a271f3f7a40632073bbdb9b889a
Александр Родионов <rexarrior@yandex.ru>
2023-10-08 23:58:43 +0300

    add letsencrypt sert in deploy

--- deploy/gunicorn_config.py
+++ deploy/gunicorn_config.py
@@ -5,3 +5,6 @@ pythonpath = '/root/django-tailwind-blog/env/bin/python3'
 bind = '0.0.0.0:80'
 
 workers = 4
+
+certfile='/etc/letsencrypt/live/helenkapatsa.ru/fullchain.pem'
+keyfile='/etc/letsencrypt/live/helenkapatsa.ru/privkey.pem'
--- deploy/install.sh
+++ deploy/install.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
-apt-get update; apt-get install -y git python3 python3-pip python3-venv;
+#install packages
+apt-get update; apt-get install -y git python3 python3-pip python3-venv software-properties-common;
+
+#getting letsencrypt sertificate
+python3 -m pip install certbot
+#user actions required
+certbot certonly --standalone --preferred-challenges http -d helenkapatsa.ru
+
+#getting and setup project
 git clone https://github.com/fitwist/django-tailwind-blog ;
 cd django-tailwind-blog/;
 python3 -m venv env;
 source env/bin/activate;
 python3 -m pip install -r requirements.txt;
 deactivate;
+
+#setup systemd
 cp deploy/blog_app_server.service  /etc/systemd/system/;
 systemctl daemon-reload;
 systemctl start blog_app_server;
 systemctl enable blog_app_server;
-
 
 
 
